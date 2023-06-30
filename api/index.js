f94a2e02c67caac923bf53cae0a543472f91d463
Nabeel Akbar <studiosnabeel@gmail.com>
2023-06-30 14:22:54 +0500

    Installed nodemon again in the right folder

--- api/index.js
+++ api/index.js
@@ -3,5 +3,5 @@ import express from 'express';
 const app = express();
 
 app.listen(5000, () => {
-  console.log('Connected to port 5000 successfully!');
+  console.log('Connected to port 5000 successfully');
 });
