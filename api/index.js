a5e29ff24943d1a029ae5996362ebac22f1f7bf4
Nabeel Akbar <studiosnabeel@gmail.com>
2023-07-01 01:51:12 +0500

    creating basic route endpoints for all

--- api/index.js
+++ api/index.js
@@ -2,6 +2,9 @@ import express from 'express';
 import dotenv from 'dotenv';
 import mongoose from 'mongoose';
 import authRoute from './routes/auth.js';
+import usersRoute from './routes/users.js';
+import hotelsRoute from './routes/hotels.js';
+import roomsRoute from './routes/rooms.js';
 
 const app = express();
 dotenv.config();
@@ -16,8 +19,10 @@ const connect = async () => {
 };
 
 //middleware
-
 app.use('/api/auth', authRoute);
+app.use('/api/users', usersRoute);
+app.use('/api/hotels', hotelsRoute);
+app.use('/api/rooms', roomsRoute);
 
 mongoose.connection.on('disconnected', () => {
   console.log('mongoDB disconnected');
