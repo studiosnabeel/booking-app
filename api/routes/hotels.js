2a95742329532d74fb29ab2e24a04ca821d5fc03
Nabeel Akbar <studiosnabeel@gmail.com>
2023-07-01 18:03:34 +0500

    Create Schema for Hotels

--- api/routes/hotels.js
+++ api/routes/hotels.js
@@ -2,4 +2,13 @@ import express from 'express';
 
 const router = express.Router();
 
+//CREATE
+router.post('/', async (req,res)=> {
+
+})
+//UPDATE
+//DELETE
+//GET
+//GETALL
+
 export default router;
