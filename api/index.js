fd4e20b438cb0a95fcbd209ff726f14b88885d0c
Nabeel Akbar <studiosnabeel@gmail.com>
2023-07-02 01:57:29 +0500

    Deleting the middleware that was fr learning purps

--- api/index.js
+++ api/index.js
@@ -24,16 +24,13 @@ mongoose.connection.on('disconnected', () => {
 
 //middleware
 
+
 app.use(express.json());
 
 app.use('/api/auth', authRoute);
 app.use('/api/users', usersRoute);
 app.use('/api/hotels', hotelsRoute);
 app.use('/api/rooms', roomsRoute);
-
-app.use((req, res, next) => {
-  res.send('Hello world!')
-});
 
 app.listen(5000, () => {
   connect();
