e9c986b5b084c358b8e217ddd382eb00fca71d66
Nabeel Akbar <studiosnabeel@gmail.com>
2023-07-02 00:43:11 +0500

    Delete Method for Deleting a Hotel

--- api/routes/hotels.js
+++ api/routes/hotels.js
@@ -28,8 +28,17 @@ router.put('/:id', async (req, res) => {
     res.status(500).json(err);
   }
 });
+
 //DELETE
-//GET
-//GETALL
+router.delete('/:id', async (req, res) => {
+  try {
+    await Hotel.findByIdAndDelete(req.params.id);
+    res.status(200).json('Hotel has been deleted');
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+
 
 export default router;
