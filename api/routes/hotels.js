053a6f551a0a1f3ba77c5ee787328196713763c0
Nabeel Akbar <studiosnabeel@gmail.com>
2023-07-01 22:10:17 +0500

    Add Update method for Hotels

--- api/routes/hotels.js
+++ api/routes/hotels.js
@@ -16,7 +16,18 @@ router.post('/', async (req, res) => {
 });
 
 //UPDATE
-
+router.put('/:id', async (req, res) => {
+  try {
+    const updatedHotel = await Hotel.findByIdAndUpdate(
+      req.params.id,
+      { $set: req.body },
+      { new: true }
+    );
+    res.status(200).json(updatedHotel);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
 //DELETE
 //GET
 //GETALL
