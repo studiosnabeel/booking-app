ca431d64a826f6790e4e622e545bec6f8c9678bd
Nabeel Akbar <studiosnabeel@gmail.com>
2023-07-02 20:56:04 +0500

    Hash password with bcryptjs nodjs library

--- api/controllers/auth.js
+++ api/controllers/auth.js
@@ -1,11 +1,15 @@
 import User from '../models/User.js';
+import bcrypt from 'bcryptjs';
 
 export const register = async (req, res, next) => {
   try {
+    let salt = bcrypt.genSaltSync(10);
+    let hash = bcrypt.hashSync(req.body.password, salt);
+
     const newUser = new User({
       username: req.body.username,
       email: req.body.email,
-      password: req.body.password,
+      password: hash,
     });
 
     await newUser.save();
