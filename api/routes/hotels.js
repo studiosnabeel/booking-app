bd5884d0ac4b795efa87d6b58bbd3a4702d9cad1
Nabeel Akbar <studiosnabeel@gmail.com>
2023-07-02 14:22:53 +0500

    Remove the error testing code

--- api/routes/hotels.js
+++ api/routes/hotels.js
@@ -54,10 +54,8 @@ router.get('/:id', async (req, res) => {
 router.get('/', async (req, res, next) => {
   const failed = true;
 
-  if (failed) return next(createError(401, 'You are not authenticated'));
-
   try {
-    const hotels = await Hotel.findById(234324);
+    const hotels = await Hotel.find();
     res.status(200).json(hotels);
   } catch (err) {
     next(err);
