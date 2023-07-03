90047082b3063db42c0133afaae8823abd26cd74
Nabeel Akbar <studiosnabeel@gmail.com>
2023-07-03 20:08:55 +0500

    Add verifications to routes as needed

--- api/routes/users.js
+++ api/routes/users.js
@@ -9,28 +9,28 @@ import { verifyAdmin, verifyToken, verifyUser } from '../utils/verifyToken.js';
 
 const router = express.Router();
 
-router.get('/checkauthentication', verifyToken, (req, res, next) => {
-  res.send('hello user, your logged in');
-});
+// router.get('/checkauthentication', verifyToken, (req, res, next) => {
+//   res.send('hello user, your logged in');
+// });
 
-router.get('/checkuser/:id', verifyUser, (req, res, next) => {
-  res.send('hello user, your logged in and you can delete your account');
-});
+// router.get('/checkuser/:id', verifyUser, (req, res, next) => {
+//   res.send('hello user, your logged in and you can delete your account');
+// });
 
-router.get('/checkadmin/:id', verifyAdmin, (req, res, next) => {
-  res.send('hello Admin, your logged in and you can delete all account');
-});
+// router.get('/checkadmin/:id', verifyAdmin, (req, res, next) => {
+//   res.send('hello Admin, your logged in and you can delete all account');
+// });
 
 //UPDATE
-router.put('/:id', updateUser);
+router.put('/:id', verifyUser, updateUser);
 
 //DELETE
-router.delete('/:id', deleteUser);
+router.delete('/:id', verifyUser, deleteUser);
 
 //GET
-router.get('/:id', getUser);
+router.get('/:id', verifyUser, getUser);
 
 //GETALL
-router.get('/', getUsers);
+router.get('/', verifyAdmin, getUsers);
 
 export default router;
