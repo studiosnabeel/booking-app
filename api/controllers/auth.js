a83b25ec42e20be25a02a442d6d13cd849f51e7c
Nabeel Akbar <studiosnabeel@gmail.com>
2023-07-03 13:27:28 +0500

    import jwt

--- api/controllers/auth.js
+++ api/controllers/auth.js
@@ -1,6 +1,7 @@
 import User from '../models/User.js';
 import bcrypt from 'bcryptjs';
 import { createError } from '../utils/error.js';
+import jwt from 'jsonwebtoken';
 
 export const register = async (req, res, next) => {
   try {
@@ -31,6 +32,14 @@ export const login = async (req, res, next) => {
     );
     if (!isPasswordCorrect)
       return next(createError(400, 'Wrong password or username.'));
+
+    const token = jwt.sign(
+      {
+        id: user._id,
+        isAdmin: user.isAdmin,
+      },
+      process.env.JWT_SECRET
+    );
 
     const { password, isAdmin, ...otherDetails } = user._doc;
     res.status(200).json(otherDetails);
