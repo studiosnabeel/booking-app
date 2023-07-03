e2961316d0997ed6492553b3b4442a7b928b4a9a
Nabeel Akbar <studiosnabeel@gmail.com>
2023-07-04 00:20:57 +0500

    Modify room controller and routes

--- api/controllers/room.js
+++ api/controllers/room.js
@@ -1,20 +1,21 @@
 import Room from '../models/Room.js';
+import Hotel from '../models/Hotel.js';
 import { createError } from '../utils/error.js';
 
 export const createRoom = async (req, res, next) => {
-  const RoomId = req.params.Roomid;
+  const hotelId = req.params.hotelid;
   const newRoom = new Room(req.body);
 
   try {
     const savedRoom = await newRoom.save();
     try {
-      await Room.findByIdAndUpdate(RoomId, {
+      await Hotel.findByIdAndUpdate(hotelId, {
         $push: { rooms: savedRoom._id },
       });
     } catch (err) {
       next(err);
     }
-    res.status(200).json(savedRoom)
+    res.status(200).json(savedRoom);
   } catch (err) {
     next(err);
   }
@@ -34,11 +35,20 @@ export const updateRoom = async (req, res, next) => {
 };
 
 export const deleteRoom = async (req, res) => {
+  const hotelId = req.params.hotelid;
+
   try {
     await Room.findByIdAndDelete(req.params.id);
+    try {
+      await Hotel.findByIdAndUpdate(hotelId, {
+        $pull: { rooms: req.params.id },
+      });
+    } catch (err) {
+      next(err);
+    }
     res.status(200).json('Room has been deleted');
   } catch (err) {
-    res.status(500).json(err);
+    next(err);
   }
 };
 
