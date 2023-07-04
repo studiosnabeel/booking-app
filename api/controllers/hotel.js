56c81f4b4261f030e7d48a0c0a2feffcbd99db35
Nabeel Akbar <studiosnabeel@gmail.com>
2023-07-04 14:45:58 +0500

    Create countByCity contrller method

--- api/controllers/hotel.js
+++ api/controllers/hotel.js
@@ -43,9 +43,26 @@ export const getHotel = async (req, res) => {
 };
 
 export const getHotels = async (req, res, next) => {
+  const { min, max, ...others } = req.query;
   try {
-    const hotels = await Hotel.find();
+    const hotels = await Hotel.find({
+      ...others,
+      cheapestPrice: { $gt: min | 1, $lt: max || 999 },
+    }).limit(req.query.limit);
     res.status(200).json(hotels);
+  } catch (err) {
+    next(err);
+  }
+};
+export const countByCity = async (req, res, next) => {
+  const cities = req.query.cities.split(',');
+  try {
+    const list = await Promise.all(
+      cities.map((city) => {
+        return Hotel.countDocuments({ city: city });
+      })
+    );
+    res.status(200).json(list);
   } catch (err) {
     next(err);
   }
