69772b45cc4faf70647382d44ef42c558a0747da
Nabeel Akbar <studiosnabeel@gmail.com>
2023-07-07 00:22:56 +0500

    Route for logout and also modify Navbar

--- client/src/components/navbar/Navbar.jsx
+++ client/src/components/navbar/Navbar.jsx
@@ -1,17 +1,47 @@
+import { useContext } from 'react';
 import './navbar.css';
 import { Link } from 'react-router-dom';
+import { AuthContext } from '../../context/AuthContext';
+import { useNavigate } from 'react-router-dom';
 
 const Navbar = () => {
+  const navigate = useNavigate();
+  const { user, dispatch } = useContext(AuthContext);
+
+  const logout = () => {
+    dispatch({ type: 'LOGOUT' });
+  };
+
   return (
     <div className="navbar">
       <div className="navContainer">
         <Link to="/" style={{ color: ' inherit', textDecoration: 'none' }}>
           <span className="logo">NabeelBooking</span>
         </Link>
-        <div className="navItems">
-          <button className="navButton">Register</button>
-          <button className="navButton">Login</button>
-        </div>
+        {user ? (
+          <>
+            <div className="navItems">
+              {user.username}
+              <button className="navButton" onClick={logout}>
+                Logout
+              </button>
+            </div>
+          </>
+        ) : (
+          <div className="navItems">
+            <button className="navButton" onClick={()=>{navigate('/register')}}>
+              Register
+            </button>
+            <button
+              className="navButton"
+              onClick={() => {
+                user ? navigate('/') : navigate('/login');
+              }}
+            >
+              Login
+            </button>
+          </div>
+        )}
       </div>
     </div>
   );
