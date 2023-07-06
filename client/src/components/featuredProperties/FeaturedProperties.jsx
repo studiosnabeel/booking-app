0707e137516d5dc81a1647b30c98ef77fc5260d7
Nabeel Akbar <studiosnabeel@gmail.com>
2023-07-07 00:52:17 +0500

    Add photos in db for hotels and style featuredprop

--- client/src/components/featuredProperties/FeaturedProperties.jsx
+++ client/src/components/featuredProperties/FeaturedProperties.jsx
@@ -14,9 +14,11 @@ const FeaturedProperties = () => {
           {data.map((item) => (
             <div className="fpItem" key={item._id}>
               <img
-                src="https://cf.bstatic.com/xdata/images/hotel/square600/87375132.webp?k=a3eff4ea2475f3a4de01f017463acd719bddada5e63f87f6c0952f8590498865&o="
+                src={item.photos}
                 alt=""
                 className="fpImg"
+                width={200}
+                height={150}
               />
               <span className="fpName">{item.name}</span>
               <span className="fpCity">{item.city}</span>
